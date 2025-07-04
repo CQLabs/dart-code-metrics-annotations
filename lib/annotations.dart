4427ecc0fca2e1e3d9a72f315e46b3d33305a350
Dmitry Zhifarsky <dimannich2380@gmail.com>
2025-07-04 16:31:02 +0400

    feat: add new declaration for pass-correct-accepted-type

--- lib/annotations.dart
+++ lib/annotations.dart
@@ -46,3 +46,39 @@ class Throws {
   /// or any exception if the [expectedErrors] is not specified.
   const Throws([this.expectedErrors]);
 }
+
+/// Used to annotate fields or parameters that have a type `Object`,
+/// but should only receive a known subset of types (e.g. `String` and `int`).
+///
+/// The intention of this annotation is to help avoid passing an incorrect
+/// or unexpected value.
+///
+/// For example:
+///
+/// ```dart
+/// void fn() {
+///  fn(1); // LINT
+///  SomeClass(false); // LINT
+///}
+///
+///void fn(@AcceptedTypes({String, bool}) Object value) {}
+///
+///class SomeClass {
+///  @AcceptedTypes({String, int})
+///  final Object field;
+///
+///  const SomeClass(this.field);
+///}
+/// ```
+///
+/// DCM will provide feedback if an invocation receives an argument that does not match the type listed in the `@AcceptedTypes()`.
+///
+/// For more information, see the documentation for "pass-correct-accepted-types".
+class AcceptedTypes {
+  /// The types which the annotated declaration accepts.
+  final Set<Type>? types;
+
+  /// Create a new instance of [AcceptedTypes] to be used as an annotation on a declaration
+  /// that have an `Object` type, but should only receive a known subset of types.
+  const AcceptedTypes(this.types);
+}
